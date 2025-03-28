ab574d852bc78bff83a1836f74ed618fdc0a15b9
GUILHERME RIBEIRO SOARES <guilherme.rsoares@a.ucb.br>
2025-03-28 10:22:23 -0300

    sync das atividades em salça

---
+++ atividade-1/variaveisOperadores.js
@@ -0,0 +1,22 @@
+const num_A = 5;
+const num_B = 5;
+
+let operacao = 'soma';
+let resultado = 5 + 5;
+
+console.log(`Resultado da ${operacao} entre ${num_A} e ${num_B} é ${resultado}`);
+
+operacao = 'subtração';
+resultado = 5 - 5;
+
+console.log(`Resultado da ${operacao} entre ${num_A} e ${num_B} é ${resultado}`);
+
+operacao = 'multiplicação';
+resultado = 5 * 5;
+
+console.log(`Resultado da ${operacao} entre ${num_A} e ${num_B} é ${resultado}`);
+
+operacao = 'divisão';
+resultado = 5 / 5;
+
+console.log(`Resultado da ${operacao} entre ${num_A} e ${num_B} é ${resultado}`);

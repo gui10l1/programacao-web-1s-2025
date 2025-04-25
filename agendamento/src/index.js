2295243bd80b1931cd005f39260b14e122adeba5
GUILHERME RIBEIRO SOARES <guilherme.rsoares@a.ucb.br>
2025-04-25 10:12:20 -0300

    add: appointments form project

---
+++ agendamento/src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const path = require('path');
+const routes = require('./routes/index.routes');
+
+const VIEWS_FOLDER = path.join(__dirname, '..', 'views');
+
+const app = express();
+
+app.engine('html', mustacheExpress());
+
+app.set('view engine', 'html');
+app.set('views', VIEWS_FOLDER);
+
+app.use(express.static(path.join(__dirname, '..', 'public' )));
+
+app.use(routes);
+
+app.listen(3000, () => console.log('Server started on port 3000'));
